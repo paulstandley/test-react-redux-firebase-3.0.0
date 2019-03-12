6034fa0f148f02241285ba2b6b84ef3169b7cb77
Paul Standley <paulstandley1972@gmail.com>
2019-03-12 22:02:49 +0000

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+// import 'firebase/firestore' // <- needed if using firestore
+// import 'firebase/functions' // <- needed if using httpsCallable
+import { createStore, combineReducers, compose } from 'redux'
+import { ReactReduxFirebaseProvider, firebaseReducer } from 'react-redux-firebase'
+// import { createFirestoreInstance, firestoreReducer } from 'redux-firestore' // <- needed if using firestore
+import App1 from './App1'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const fbConfig = {
+  apiKey: "API_KEY",
+  authDomain: "project-planer-8d7d7.firebaseapp.com",
+  databaseURL: "https://project-planer-8d7d7.firebaseio.com",
+  projectId: "project-planer-8d7d7",
+  storageBucket: "project-planer-8d7d7.appspot.com",
+  messagingSenderId: "897259823180"
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users',
+  // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+}
+
+// Initialize firebase instance
+firebase.initializeApp(fbConfig)
+
+// Initialize other services on firebase instance
+// firebase.firestore() // <- needed if using firestore
+// firebase.functions() // <- needed if using httpsCallable
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  // firestore: firestoreReducer // <- needed if using firestore
+})
+
+// Create store with reducers and initial state
+const initialState = {}
+const store = createStore(rootReducer, initialState)
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  // createFirestoreInstance // <- needed if using firestore
+}
+
+// Setup react-redux so that connect HOC can be used
+const App = () => (
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App1 />
+    </ReactReduxFirebaseProvider>
+  </Provider>
+);
+
+render(<App/>, document.getElementById('root'));
